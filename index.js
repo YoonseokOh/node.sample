da2e14307295767d1be806afe723959248fd2c28
Yoonseok Oh <promiseys@hotmail.com>
2017-08-12 18:10:00 +0900

    Basic project

---
+++ index.js
@@ -0,0 +1,56 @@
+/*
+ * Function : index.js
+ *
+ * Description : start up
+ *
+ * Copyright (c) 2017, Yoonseok Oh.
+ * Licensed under Yoonseok Oh
+ *
+ * Email : yoonseok.oh@icloud.com
+ *
+ */
+
+
+"use strict";
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const hbs = require('./hbs-engine');
+
+require('./app/globals/constants');
+
+// Middle-ware
+const compression = require('compression');
+const minify = require('express-minify');
+
+app.disable('x-powered-by');
+
+app.use(compression());
+app.use(minify());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Set view engine ( hbs : handlebars )
+app.set('trust proxy', 'loopback');
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/app/views');
+
+// Set pre route
+require('./app/globals/pre-route')(app);
+
+// Set routes
+const routeIndex = require('./app/routes');
+
+app.use('/', routeIndex);
+
+// Set post route
+require('./app/globals/post-route')(app);
+
+module.exports = app;
